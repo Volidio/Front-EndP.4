c086492b471a6f556345ecb36063ef8993a084d5
Volidio <davivilar1234@gmail.com>
2026-02-23 09:35:13 -0300

    teste

---
+++ relogio/script.js
@@ -0,0 +1,18 @@
+function atualizarRelogio() {
+            const agora = new Date();
+
+            let horas = agora.getHours();
+            let minutos = agora.getMinutes();
+            let segundos = agora.getSeconds();
+
+            horas = horas < 10 ? "0" + horas : horas;
+            minutos = minutos < 10 ? "0" + minutos : minutos;
+            segundos = segundos < 10 ? "0" + segundos : segundos;
+
+            document.getElementById("relogio").innerHTML =
+                `${horas}:${minutos}:${segundos}`;
+        }
+
+        setInterval(atualizarRelogio, 1000);
+
+        atualizarRelogio();
